801e320ab9ff86f34d4e3604c4828ec1e9ca4f24
SAMKOREA <123339750+Jeonmingang@users.noreply.github.com>
2025-10-27 06:15:43 +0900

    Add files via upload

--- src/main/java/com/minkang/ultimate/voteplus/AutoNoticeManager.java
+++ src/main/java/com/minkang/ultimate/voteplus/AutoNoticeManager.java
@@ -6,11 +6,6 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.entity.Player;
-import net.md_5.bungee.api.chat.TextComponent;
-import net.md_5.bungee.api.chat.ClickEvent;
-import net.md_5.bungee.api.chat.HoverEvent;
-import net.md_5.bungee.api.chat.ComponentBuilder;
-import net.md_5.bungee.api.chat.BaseComponent;
 
 
 import java.io.File;
